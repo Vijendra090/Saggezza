b97f28a9334a3d8ce2344f0f02ad22d33ccfc2c6
Vijendra Prasad <vijendra.prasad@SGZ.com>
2019-01-31 15:43:37 +0530

    updated the employee files

--- src/main/java/employee/Employee.java
+++ src/main/java/employee/Employee.java
@@ -1,4 +1,46 @@
 package employee;
 
 public class Employee {
+    private Integer id;
+    private String name;
+    private String role;
+    private String salary;
+
+    public Employee(){}
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "ID->"+getId()+"::Name->"+getName()+"::Role->"+getRole()+"::Salary->"+getSalary();
+    }
 }
--- src/main/java/employee/ReadCSVWithScanner.java
+++ src/main/java/employee/ReadCSVWithScanner.java
@@ -1,4 +1,39 @@
 package employee;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReadCSVWithScanner {
+    public static void main(String[] args) throws IOException {
+        //open file input stream
+        BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\vijendra.prasad\\IdeaProjects\\Saggezza\\src\\main\\java\\employee\\employee.csv"));
+        String line="";
+        int index;
+        List<Employee> empList = new ArrayList<Employee>();
+
+        while ((line=reader.readLine())!=null) {
+            //System.out.println(line);
+            index=0;
+            Employee employee =new Employee();
+
+            for(String col :line.split(",")){
+                if(index==0)
+                    employee.setId(Integer.parseInt(col));
+                if(index==1)
+                    employee.setName(col);
+                if(index==2)
+                    employee.setRole(col);
+                if(index==3)
+                    employee.setSalary(col);
+                index++;
+            }
+            empList.add(employee);
+        }
+
+        for(int i=0;i<empList.size();i++)
+            System.out.println(empList.get(i));
+    }
 }
