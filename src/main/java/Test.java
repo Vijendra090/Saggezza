0b96ee2ff5fc8ea4d043eb227120a5ba825462b0
Vijendra Prasad <vijendra.prasad@SGZ.com>
2019-04-02 17:37:45 +0530

    genrics

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,27 @@
+
+class GenNode <T>{
+    T data;
+    GenNode next;
+
+    GenNode(T data){
+        this.data= data;
+        next=null;
+    }
+}
+
 public class Test{
-    public static void main(String args[])throws Exception{
-        System.out.println(Runtime.getRuntime().availableProcessors());
+    public static void main(String[] args){
+        GenNode head;
+        GenNode temp;
+        head= new GenNode(10);
+        head.next= new GenNode('w');
+        head.next.next=new GenNode('r');
+        head.next.next.next=new GenNode(30);
+
+        temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+"-->");
+            temp=temp.next;
+        }
     }
 }
